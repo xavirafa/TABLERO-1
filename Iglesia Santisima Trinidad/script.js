43db62216f3e832f24aa9b0c74b6f5203bda70de
XAVIRAFA2 <131085530+XAVIRAFA2@users.noreply.github.com>
2024-03-10 18:03:04 -0500

    Update script.js

--- Iglesia Santisima Trinidad/script.js
+++ Iglesia Santisima Trinidad/script.js
@@ -5,18 +5,15 @@ document.addEventListener('DOMContentLoaded', function () {
 
     menuBtn.addEventListener('click', function () {
         menu.classList.toggle('show');
-        closeBtn.style.display = 'block'; // Mostrar el botón "X"
     });
 
     closeBtn.addEventListener('click', function () {
         menu.classList.remove('show');
-        closeBtn.style.display = 'none'; // Ocultar el botón "X"
     });
 
     document.addEventListener('click', function (event) {
         if (!menu.contains(event.target) && !menuBtn.contains(event.target)) {
             menu.classList.remove('show');
-            closeBtn.style.display = 'none'; // Ocultar el botón "X"
         }
     });
 });
